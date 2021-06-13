929bcc0e07071c9d38aa086bb06fa1f717cc0d3e
Sandip Rijal <sandurijal03@hotmail.com>
2021-06-13 08:44:30 +0545

    added crud basic logic

---
+++ src/controllers/taskController.js
@@ -0,0 +1,11 @@
+export const getTodos = (req, res, next) => {
+  res.status(200).json('all tasks');
+};
+
+export const createTodo = (req, res, next) => {};
+
+export const getTodo = (req, res, next) => {};
+
+export const updateTodo = (req, res, next) => {};
+
+export const removeTodo = (req, res, next) => {};
