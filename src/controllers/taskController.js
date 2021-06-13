e354e7c9b0762efaaab4ee6da18dead8e1fb5f4f
Sandip Rijal <sandurijal03@hotmail.com>
2021-06-13 20:55:43 +0545

    added updating task route and deleting task route

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -26,6 +26,7 @@ export const createTask = async (req, res, next) => {
 export const getTask = async (req, res, next) => {
   try {
     const { id } = req.params;
+
     const task = await Task.findById(id);
 
     if (!task) {
@@ -38,6 +39,33 @@ export const getTask = async (req, res, next) => {
   }
 };
 
-export const updateTask = (req, res, next) => {};
+export const updateTask = async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-export const removeTask = (req, res, next) => {};
+    const task = await Task.findByIdAndUpdate(id, req.body, { new: true });
+
+    if (!task) {
+      return res.status(404).json({ msg: 'No any task with that id' });
+    }
+
+    return res.status(200).json(task);
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
+};
+
+export const removeTask = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const task = await Task.findByIdAndDelete(id);
+
+    if (!task) {
+      return res.status(404).json({ msg: 'No any task with that id' });
+    }
+
+    return res.status(200).json({ msg: 'Task removed successfully' });
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
+};
