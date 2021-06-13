46dd101737e809680763a3b7bd961c33e869867a
Sandip Rijal <sandurijal03@hotmail.com>
2021-06-13 10:15:48 +0545

    updated tasks model

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,8 +1,17 @@
 import { Schema, model } from 'mongoose';
 
 const taskSchema = new Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, 'must provide name'],
+    maxlength: [20, 'length of name cannot be more than 20 characters'],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const Task = model('Task', taskSchema);
